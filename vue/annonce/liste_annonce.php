1677238d32b40eae6a50bb35ec777bcb72eee4b2
aladinabdelkafi <aladinabdelkafi02gmail.com>
2022-02-24 11:23:35 +0100

    update image

--- vue/annonce/liste_annonce.php
+++ vue/annonce/liste_annonce.php
@@ -1,4 +1,4 @@
-<div class="site-blocks-cover overlay" style="background-image: url(ac/images/hero_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+<div class="site-blocks-cover overlay" style="background-image: url(ac/images/vente-de-vêtement-en-ligne-01.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
   <div class="container">
     <div class="row align-items-center justify-content-center text-center">
 
